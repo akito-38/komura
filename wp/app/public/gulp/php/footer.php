06e8b0e927142e4741dbaaf3a4c485e0bd2f1434
miyamoto <m.akirabo@gmail.com>
2021-10-13 18:00:41 +0900

    フォーム作成

--- wp/app/public/gulp/php/footer.php
+++ wp/app/public/gulp/php/footer.php
@@ -1,6 +1,38 @@
-<footer class="ly_main-footer">
-
-</footer>
+<div class="max-wide">
+  <fotter class="mainFooter js-move h-before-effect">
+    <a href="#" class="upElv js-scroll"></a>
+    <div>
+      <div class="mainFooter_name">古村幹夫後援会事務所</div>
+      <div class="mainFooter_tel">
+        TEL.<a href="tel:050-3635-4956" class="mainFooter_tel_number">050-3635-4956</a>
+      </div>
+      <div class="mainFooter_fax">FAX.026-403-4943</div>
+      <div class="mainFooter_txt">&#12306;399-0423<br class="u-hide-pc"> 長野県上伊那郡辰野町大字澤底1760</div>
+      <div class="mainFooter_txt">e-mail:
+        <a href="mailto:komura.mikio@gmail.com">komura.mikio@gmail.com</a>
+      </div>
+    </div>
+    <div>
+      <div class="iconList">
+        <a href="#">
+          <img src="<?php print TMP_URL; ?>/img/icon/icon-pointer.svg" alt="" width="10" height="15" loading="lazy">
+          アクセス
+        </a>
+        <a href="#">
+          <img src="<?php print TMP_URL; ?>/img/icon/icon-facebook.svg" alt="" width="35" height="35" loading="lazy">
+        </a>
+        <a href="#">
+          <img src="<?php print TMP_URL; ?>/img/icon/icon-twitter.svg" alt="" width="35" height="35" loading="lazy">
+        </a>
+      </div>
+    </div>
+  </fotter>
+</div>
+<div class="copyWrapper js-move h-before-effect">
+  <div class="max-wide">
+    <small>&copy;komura mikio</small>
+  </div>
+</div>
 
 <?php wp_footer(); ?>
 </body>
---
+++ wp/app/public/gulp/php/page-contact.php
@@ -0,0 +1,179 @@
+<?php
+  get_header();
+?>
+<header class="pageHeader">
+  <h1>コンタクト</h1>
+  <div>Contact</div>
+</header>
+
+<div class="max-wide">
+  <div class="formSelect">
+    <label>
+      後援会入会
+      <input type="radio" name="select" checked>
+      <span class="formSelect_radio"></span>
+    </label>
+
+    <label>
+      お問い合わせ
+      <input type="radio" name="select">
+      <span class="formSelect_radio"></span>
+    </label>
+  </div>
+
+  <h2 class="pageHeader02">「古村幹夫後援会」ご入会お申込み</h2>
+  <p class="pageTxt">
+    古村幹夫の信念や政治活動にご賛同いただける皆様に古村幹夫後援会」への入会をお願い申し上げます。ご紹介者がいらっしゃる場合は、ご紹介者覧にご記入ください。入会金や年会費などは一切ございません。以下のフォームに必須項目をご記入の上、送信してください。後日、入力して頂いた連絡先（電話、またはメール）に、ご連絡させていただきます。
+  </p>
+  <small class="formSmall">は必須項目となります。</small>
+
+  <dl class="formList">
+    <div class="formList_item">
+      <dt class="formList_dt require">本人お名前（漢字）</dt>
+      <dd class="formList_dd"><input type="text" class="el_input"></dd>
+    </div>
+    <div class="formList_item">
+      <dt class="formList_dt require">本人お名前（フリガナ）</dt>
+      <dd class="formList_dd"><input type="text" class="el_input"></dd>
+    </div>
+    <div class="formList_item">
+      <dt class="formList_dt require">郵便番号</dt>
+      <dd class="formList_dd">
+        <input type="text" class="el_input el_input__223">
+        <small>
+          ハイフン無しでご入力ください。<br>
+          ※郵便番号を入力すると自動で住所が入力されます。
+        </small>
+      </dd>
+    </div>
+    <div class="formList_item">
+      <dt class="formList_dt require">住所</dt>
+      <dd class="formList_dd"><input type="text" class="el_input"></dd>
+    </div>
+    <div class="formList_item">
+      <dt class="formList_dt require">電話番号</dt>
+      <dd class="formList_dd"><input type="text" class="el_input"></dd>
+    </div>
+
+    <div class="formList_item">
+      <dt class="formList_dt any">メールアドレス</dt>
+      <dd class="formList_dd"><input type="email" class="el_input"></dd>
+    </div>
+
+    <div class="formList_item">
+      <dt class="formList_dt any">メールアドレス（確認）</dt>
+      <dd class="formList_dd"><input type="email" class="el_input"></dd>
+    </div>
+
+    <div class="formList_item">
+      <dt class="formList_dt any">メール受信の同意</dt>
+      <dd class="formList_dd">
+        メールの受信に同意する
+        <input type="radio">
+      </dd>
+    </div>
+
+    <div class="formList_item formList_item__mb">
+      <dt class="formList_dt any">生年月日（西暦）</dt>
+      <dd class="formList_dd">
+        <input type="text" class="el_input el_input__date">
+        年
+        <input type="text" class="el_input el_input__date">
+        月
+        <input type="text" class="el_input el_input__date">
+        日
+      </dd>
+    </div>
+
+    <div class="formList_item">
+      <dt class="formList_dt any">ご紹介者お名前</dt>
+      <dd class="formList_dd"><input type="email" class="el_input"></dd>
+    </div>
+    <div class="formList_item">
+      <dt class="formList_dt any">住所</dt>
+      <dd class="formList_dd"><input type="email" class="el_input"></dd>
+    </div>
+    <div class="formList_item">
+      <dt class="formList_dt any">電話番号</dt>
+      <dd class="formList_dd"><input type="email" class="el_input"></dd>
+    </div>
+    <div class="formList_item">
+      <dt class="formList_dt any">メールアドレス</dt>
+      <dd class="formList_dd"><input type="email" class="el_input"></dd>
+    </div>
+  </dl>
+  <div class="formTxtRight">下記の規約をご確認いただき、<br class="u-hide-pc">確認画面にお進みください。</div>
+
+  <div class="termsbox">
+    <h2 class="termsboxHeader01">古村幹夫後援会規約<br class="u-hide-pc"><span class="u-hide-sp">／</span>プライバシーポリシー</h2>
+    <section>
+      <h3 class="termsboxHeader02">古村幹夫後援会規約</h3>
+      <dl>
+        <div>
+          <dt>第１条（名称・所在地）</dt>
+          <dd>本会は、古村幹夫後援会と称し、主たる事務所を辰野町沢底に置く。</dd>
+        </div>
+        <div>
+          <dt>第２条（目的）</dt>
+          <dd>本会は、辰野町政発展のため尽力している古村幹夫氏の活動を支援することを本来の目的とする。</dd>
+        </div>
+        <div>
+          <dt>第３条（事業）</dt>
+          <dd>本会は前条の目的を達成するために講演会・報告会などの他、必要な事業を行う。</dd>
+        </div>
+        <div>
+          <dt>第４条（会員）</dt>
+          <dd>本会は、第２条の目的に賛同し、入会申込書を提出した者をもって会員とする。</dd>
+        </div>
+        <div>
+          <dt>第５条（役員）</dt>
+          <dd>本会に次の役員を置く。<br>会長１名、副会長 若干名、事務局長１名、会計２名、監事２名</dd>
+        </div>
+        <div>
+          <dt>第６条（会議）</dt>
+          <dd>会長は、必要に応じ総会・役員会を招集する。</dd>
+        </div>
+        <div>
+          <dt>第７条（経費）</dt>
+          <dd>本会の経費は、寄付金・その他の収入を持って充てる。</dd>
+        </div>
+        <div>
+          <dt>第８条（会計年度及び監査）</dt>
+          <dd>会計年度は１月１日から１２月３１日までとする。会計は年末に監事による監査を受け役員会に報告する。</dd>
+        </div>
+        <div>
+          <dt>第９条（補則）</dt>
+          <dd>本規約に定めのない事項については、役員会で決定する。</dd>
+        </div>
+        <div>
+          <dt>附則</dt>
+          <dd>本規約は、令和３年９月１０日より実施する。</dd>
+        </div>
+      </dl>
+    </section>
+    <section>
+      <h3 class="termsboxHeader02">プライバシーポリシー</h3>
+
+      <p class="termsbox_txt">
+        ご記入いただいた個人情報は入会の受付、業務遂行に必要な範囲で行う業務提携先等への提供に利用します。<br>
+        当会の活動通知を目的とし、それ以外の目的に利用されることはございません。<br>
+        個人情報は厳重に管理し、利用目的の達成に必要な範囲内で業者に委託する場合は、秘密を保持させるために適切な監督を行います。
+      </p>
+      <p class="termsbox_txt">
+        古村幹夫後援会事務所<br class="u-hide-pc"><span class="u-hide-sp">／</span>後援会長　古村 仁士<br>
+        TEL.050-3635-4956 FAX.026-403-4943<br>
+        〒399-0423<br class="u-hide-pc"> 長野県上伊那郡辰野町大字澤底1760
+      </p>
+    </section>
+  </div>
+  <div class="formAgree">
+    同意する
+    <input type="checkbox">
+  </div>
+  <div class="btnWrapper">
+    <input type="submit" class="formSend" value="確認画面へ >">
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
